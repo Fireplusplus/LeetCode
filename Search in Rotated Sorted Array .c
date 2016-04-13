6dbb337c03b5e84d3aab95dc27df2aa03aaf36f3
Fireplusplus <1259039012@qq.com>
2016-04-13 08:44:15 +0800

    Update Search in Rotated Sorted Array .c

--- Search in Rotated Sorted Array .c
+++ Search in Rotated Sorted Array .c
@@ -1,9 +1,15 @@
-/* leetcode, Search in Rotated Sorted Array  */
+/*
+ * leetcode, Search in Rotated Sorted Array  
+ * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * You may assume no duplicate exists in the array.
+ */
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
-int search(int* nums, int numsSize, int target)
+int search(int* nums, int numsSize, int target)		//二分查找，难度主要在于左右边界的确定
 {
 	int first = 0;
 	int last = numsSize - 1;
